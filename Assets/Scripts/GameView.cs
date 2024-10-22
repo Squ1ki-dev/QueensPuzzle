26d2c780d3adbbdea2455e20414ae55b7f6d1d55
Squ1ki <devunit46@gmail.com>
2024-10-22 11:47:27 +0100

    fix

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Tools;
 
 public class GameView : MonoBehaviour
 {
     [SerializeField] private TMP_Text levelTxt;
     private int levelIdx;
 
-    void Start()
+    private void Start()
     {
         LevelModel model = new LevelModel(levelIdx);
         levelTxt.text = "Level: " + model.levelIdx.ToString();
+        WindowManager.Instance.CloseAll();
     }
 }
